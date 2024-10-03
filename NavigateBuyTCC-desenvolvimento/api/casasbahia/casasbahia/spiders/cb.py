bd0eecb3d65346b331897a87eefcfc1c40df8f90
joaop0102 <joaopedrosilvadeoliveira56@gmail.com>
2024-10-03 09:12:19 -0300

    scrapy casas bahia feito, faltando apenas alguns ajustes no scrapy da loja renner

--- NavigateBuyTCC-desenvolvimento/api/casasbahia/casasbahia/spiders/cb.py
+++ NavigateBuyTCC-desenvolvimento/api/casasbahia/casasbahia/spiders/cb.py
@@ -2,34 +2,37 @@ import scrapy
 
 class CasasBSpider(scrapy.Spider):
     name = 'cb'
-    start_urls = ['https://www.casasbahia.com.br/pulseira/b']
+    start_urls = ['https://www.casasbahia.com.br/carregador/b']
 
     custom_settings = {
-    'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'ROBOTSTXT_OBEY': False,
+        'DOWNLOAD_DELAY': 2,
+        'AUTOTHROTTLE_ENABLED': True,
+        'COOKIES_ENABLED': True,
+        'RETRY_ENABLED': True,
+        'RETRY_TIMES': 3,
+        'RETRY_HTTP_CODES': [403, 500, 502, 503, 504, 522, 524, 408],
+        'DEFAULT_REQUEST_HEADERS': {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Referer': 'https://www.google.com/',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+        }
     }
 
-
-
     def parse(self, response):
-        for i in response.xpath('//div[@class="css-329s0i"]'):
+        for i in response.xpath('//div[@class="css-1enexmx"]'):
             product_link = i.xpath('.//a[@class="dsvia-link-overlay css-1ogn60p"]/@href').get(default='').strip()
             product_image = i.xpath('.//img[@class="product-card__image"]/@src').get(default='').strip()
-            product_title = i.xpath('.//h3[@class="product-card__title"]/text()').get(default='').strip()
-            price_original = i.xpath('.//div[@dclass="product-card__highlight-price"]/text()').get(default='').strip()
-            price_value = i.xpath('.//span[@class="css-1vmkvrm"]/text()').get(default='').strip()
+            product_title = i.xpath('.//span[@aria-hidden="true"]/text()').get(default='').strip()
+            price_value = i.xpath('.//b[@aria-hideen="true"]/text()').get(default='').strip()
             stars = i.xpath('.//span[@class="css-1vmkvrm"]/text()').get(default='').strip()
 
-            if price_value:
-                yield {
-                    'Preço com Desconto': price_value,
-                    'Título': product_title,
-                    'Link do Produto': product_link,
-                    'Estrelas': stars,
-                    'Imagem do Produto': product_image
-                }
-            else:
-                yield {
-                    'Preço Original': price_original,
+            yield {
+                    'Preço do produto': price_value,
                     'Título': product_title,
                     'Link do Produto': product_link,
                     'Estrelas': stars,
